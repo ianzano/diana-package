d6d5c41946f2ece699e415049e0c694a6af36c2d
Antonio Ianzano <antonio@ianzano.me>
2024-03-18 03:24:15 +0100

    first commit

---
+++ src/SamplePackage.php
@@ -0,0 +1,14 @@
+<?php
+
+class SamplePackage
+{
+    public function register() // TODO: Dependency injection here
+    {
+        // $router->loadRoutes(); // load additional routes to the router
+    }
+
+    public function boot()
+    {
+
+    }
+}
