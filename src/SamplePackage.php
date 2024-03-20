3ab40098e751c60f96f6516e037e308da1655440
Antonio Ianzano <antonio@ianzano.me>
2024-03-20 19:29:34 +0100

    lifecycle update

--- src/SamplePackage.php
+++ src/SamplePackage.php
@@ -1,13 +1,20 @@
 <?php
 
+use Diana\Kernel\Kernel;
+use Diana\Routing\RoutingPackage;
 use Diana\Runtime\Application;
 use Diana\Runtime\Package;
+use Diana\Support\Debug;
 
 class SamplePackage extends Package
 {
     public function __construct(Application $app)
     {
         $app->registerController(TestController::class);
+    }
+
+    public function register(): void
+    {
     }
 
     public function boot(): void
